b7770d5793befc5fc08fe5fbde48f027fc1916c6
jay savani <jaysavani604@gmail.com>
2023-03-29 13:39:31 +0200

    Initial Commit

---
+++ autotsforecastML.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""autotsforecastML.py
+
+Structure:
+
+class a:
+    methode1():
+        submethode():
+            # some code
+            # some code 
+        # some code
+        
+    methode2():
+        submethode():
+            # some code
+            # some code
+        # some code
+        .
+        .
+        .
+    methode3():
+        submethode():
+            # some code
+            # some code
+        # some code
+"""
+
